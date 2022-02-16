d2c9c56c86caf6354bf8cee3c8c3da3b4dbad1d0
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2022-02-16 19:54:16 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -54,7 +54,7 @@ def predict_salary(YearsExperience,interview_score):
 def main():
     st.title("LET'S PREDICT YOUR SALARY")
     html_temp = """
-    <div style="background-color:Aqua;padding:10px">
+    <div style="background-color:Vista Blue;padding:10px">
     <h2 style="color:white;text-align:center;">SALARY PREDICTION ML App </h2>
     </div>
     """
